76ce08a2c75fc52d73a0c9a2cf965b655c439195
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-01-17 09:47:16 +0000

    Implement ScpiParameterParser for ScpiRequest

--- src/parameter_parser.rs
+++ src/parameter_parser.rs
@@ -1,7 +1,9 @@
 use std::str;
 use std::str::FromStr;
 
-use nom::{digit, IResult, rest};
+use nom::{digit, ErrorKind, IResult, rest};
+
+use super::request::ScpiRequest;
 
 pub trait ScpiParameterParser {
     fn parse(input: &[u8]) -> IResult<&[u8], Self>
@@ -53,5 +55,27 @@ impl ScpiParameterParser for String {
         );
 
         parser(input)
+    }
+}
+
+impl<T> ScpiParameterParser for T
+where
+    T: ScpiRequest,
+{
+    fn parse(input: &[u8]) -> IResult<&[u8], Self> {
+        named!(parser(&[u8]) -> &str,
+            map_res!(rest, str::from_utf8)
+        );
+
+        match parser(input) {
+            IResult::Done(input, string) => {
+                match Self::decode(string) {
+                    Some(instance) => IResult::Done(input, instance),
+                    None => IResult::Error(ErrorKind::MapRes),
+                }
+            }
+            IResult::Error(error) => IResult::Error(error),
+            IResult::Incomplete(needed) => IResult::Incomplete(needed),
+        }
     }
 }
